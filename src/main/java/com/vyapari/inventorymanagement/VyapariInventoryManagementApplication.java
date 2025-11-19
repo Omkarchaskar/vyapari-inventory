27e7ab44368e86795b4d6427d0f4568b8fcad229
Omkarchaskar <chaskaromkar23@gmail.com>
2025-11-19 16:30:24 +0530

    fix folder structure for deployement

---
+++ src/main/java/com/vyapari/inventorymanagement/VyapariInventoryManagementApplication.java
@@ -0,0 +1,14 @@
+package com.vyapari.inventorymanagement;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyapariInventoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyapariInventoryManagementApplication.class, args);
+	}
+
+}
